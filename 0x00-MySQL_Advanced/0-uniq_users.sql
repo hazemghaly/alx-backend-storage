1c52d49e38bf438bf6bd7f71fb254dc62575f3fd
Hazem Ghaly <125504512+hazemghaly@users.noreply.github.com>
2024-03-21 16:04:15 +0200

    Update 0-uniq_users.sql

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -2,7 +2,6 @@
 -- create table
 CREATE DATABASE IF NOT EXISTS holberton;
 USE holberton;
-GRANT ALL PRIVILEGES ON holberton.* TO 'root'@'localhost';
 CREATE TABLE IF NOT EXISTS holberton.users (
     id INT(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
     email VARCHAR(255) UNIQUE NOT NULL,

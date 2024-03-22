688f7ca60519c39f30926ef551cbb59122a4399b
Hazem Ghaly <125504512+hazemghaly@users.noreply.github.com>
2024-03-22 02:40:30 +0200

    Update 2-fans.sql

--- 0x00-MySQL_Advanced/2-fans.sql
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -1,7 +1,5 @@
 -- greating table to save users data
 -- arrange table
-CREATE DATABASE IF NOT EXISTS holberton;
-USE holberton;
 SELECT origin, COUNT(*) AS nb_fans
 FROM metal_bands
 GROUP BY origin

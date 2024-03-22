9a2cd16cb51151f91c996fd50452a25e98c5a5d4
Hazem Ghaly <125504512+hazemghaly@users.noreply.github.com>
2024-03-22 02:16:28 +0200

    Update 0-uniq_users.sql

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,8 +1,8 @@
 -- greating table to save users data
 -- create table
-CREATE DATABASE IF NOT EXISTS holberton;
-USE holberton;
-CREATE TABLE IF NOT EXISTS holberton.users (
+-- CREATE DATABASE IF NOT EXISTS holberton;
+-- USE holberton;
+CREATE TABLE IF NOT EXISTS users (
     id INT(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
     email VARCHAR(255) UNIQUE NOT NULL,
     name VARCHAR(255) UNIQUE

5b08697f6a19e12ebc326d42e23800c9983ac877
Hazem Ghaly <125504512+hazemghaly@users.noreply.github.com>
2024-03-23 01:07:12 +0200

    Update 2-fans.sql

--- 0x00-MySQL_Advanced/2-fans.sql
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -1,6 +1,6 @@
 -- greating table to save users data
 -- arrange table
-SELECT origin, COUNT(*) AS nb_fans
+SELECT origin, SUM(fans) AS nb_fans
 FROM metal_bands
 GROUP BY origin
 ORDER BY nb_fans DESC;

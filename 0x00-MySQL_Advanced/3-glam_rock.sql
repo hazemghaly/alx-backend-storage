08bbae02f9920450f247f000cac31d48ad4f38ff
Hazem Ghaly <125504512+hazemghaly@users.noreply.github.com>
2024-03-22 03:15:44 +0200

    Update 3-glam_rock.sql

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -4,5 +4,6 @@ SELECT
     band_name,
     IFNULL(2022 - formed, 0) - IFNULL(2022 - split, 0) AS lifespan
 FROM metal_bands
--- GROUP BY band_name
+WHERE
+    main_style = 'Glam rock'
 ORDER BY lifespan DESC;

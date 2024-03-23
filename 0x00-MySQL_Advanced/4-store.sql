f68d725ed284aba49d34964100ff0aea2b12d4b3
Hazem Ghaly <125504512+hazemghaly@users.noreply.github.com>
2024-03-23 03:47:10 +0200

    Add files via upload

--- 0x00-MySQL_Advanced/4-store.sql
+++ 0x00-MySQL_Advanced/4-store.sql
@@ -1,11 +1,9 @@
 -- create trigger
 -- updated
-CREATE TRIGGER decrease_quantity
-AFTER INSERT ON orders
-FOR EACH ROW
-BEGIN
-    UPDATE items
-    SET quantity = quantity - NEW.quantity
-    WHERE item_id = NEW.item_id;
-END;
-
+CREATE TRIGGER MyTrigger
+    AFTER INSERT ON orders
+    FOR EACH ROW 
+        UPDATE items
+        SET quantity = quantity - New.number
+        WHERE name = NEW.item_name;
+-- cat 4-init.sql | mysql -uroot -p holberton ; cat 4-store.sql | mysql -uroot -p holberton ;cat 4-main.sql | mysql -uroot -p holberton 

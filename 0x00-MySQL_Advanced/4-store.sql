d4e288a2ea13d73d282a0b817d1d20d9e20b2fe3
Hazem Ghaly <125504512+hazemghaly@users.noreply.github.com>
2024-03-23 02:53:41 +0200

    Add files via upload

---
+++ 0x00-MySQL_Advanced/4-store.sql
@@ -0,0 +1,11 @@
+-- create trigger
+-- updated
+CREATE TRIGGER decrease_quantity
+AFTER INSERT ON orders
+FOR EACH ROW
+BEGIN
+    UPDATE items
+    SET quantity = quantity - NEW.quantity
+    WHERE item_id = NEW.item_id;
+END;
+
